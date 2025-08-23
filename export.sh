4c9bbbbd0f9a60bb42543daf9c7054998f5a1232
Lukáš Viktora <lukas@viktora.cz>
2025-08-23 22:44:41 +0200

    change IPV4 to default

--- export.sh
+++ export.sh
@@ -2,6 +2,6 @@ export VESION=0.4.8.17-2
 export OR_PORT=2123
 export PT_PORT=2133
 export EMAIL=tor@opentree.cz
-export TOR_OR_PORT_IPV4=0
+export TOR_OR_PORT_IPV4=1
 export TOR_EXITRELAY=0
 export TOR_BRIDGERELAY=1
